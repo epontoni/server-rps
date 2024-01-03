ea3c0bb98e519edea16e1f5506fe8612e4099022
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2024-01-03 14:03:15 -0300

    Creation of a 'room' resource with nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [],
+  imports: [RoomModule],
   controllers: [AppController],
   providers: [AppService],
 })
